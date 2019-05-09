965d8d87ef375e76dce208bc70c5d99e947283ab
Anthony Wildman <anthonywildman@hotmail.co.uk>
2019-05-09 18:50:58 +0100

    Functionality works except if the user has used too many correct letters

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,30 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
 
   def new
-    alphabet = ('a'..'z').to_a
-    @letters = 10.times.map { (alphabet[rand(0..25)]) }
-
+    @letters = Array.new(10) { ('a'..'z').to_a[rand(0..25)] }
   end
 
   def score
+    true_words
     sorted_grid = params[:letters].split
     sorted_player_word = params[:player].split(//)
-      if (sorted_player_word - sorted_grid).empty?
-        @message = "Well done"
 
+      if (sorted_player_word - sorted_grid).empty? == false
+        @message = "Sorry, '#{params[:player]}' can't be made from #{params[:letters]}"
+      elsif @words['found'] == false
+        @message = "Sorry, but '#{params[:player]}' does not seem to be a valid English word..."
+      elsif (sorted_player_word - sorted_grid).empty? && @words['found'] == true
+        @message = "Well done, '#{params[:player]}' is a great word!"
       end
-    # raise
+
+  end
+
+  private
+
+  def true_words
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:player]}"
+    @words = JSON.parse(open(url).read)
   end
 end
